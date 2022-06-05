1740ada6b423ef4a6153408233c4a7d67fa32435
Akash Yadav <81186992+AK-aShH@users.noreply.github.com>
2022-06-05 14:04:46 +0530

    update app.js

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.post("/success", function (req, res) {
     res.redirect("/");
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT ||  3000, () => {
     console.log('Server started on port 3000');
     });  
 
